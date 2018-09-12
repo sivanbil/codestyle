76e76d125f4b93c13ff10c119a4d030d29bd025d
Sivan Liao <416202337@qq.com>
2018-09-12 11:50:29 +0800

    Update countplane.php

--- countplane.php
+++ countplane.php
@@ -1,9 +1,9 @@
 <?php
 // interval序列
 $plane_time_list = [[1, 10], [2, 3], [4, 6], [5, 9],[4, 4]];
-echo compute_plane_nums_intime($plane_time_list, false);
+echo compute_plane_nums_meanwhile($plane_time_list, false);
 
-function compute_plane_nums_intime($plane_time_list = [])
+function compute_plane_nums_meanwhile($plane_time_list = [])
 {
     if(!is_array($plane_time_list) || !$plane_time_list) {
         return 0;
@@ -12,7 +12,7 @@ function compute_plane_nums_intime($plane_time_list = [])
    
     $end = [];
   
-    $intime_nums = 0;
+    $meanwhile_nums = 0;
    
     foreach($plane_time_list as $plane_time_info) 
     {
@@ -39,8 +39,8 @@ function compute_plane_nums_intime($plane_time_list = [])
     $log = [];
     foreach($bitbuket as $num) 
     {
-        $intime_nums+=$num;
-        $log[] = $intime_nums;
+        $meanwhile_nums+=$num;
+        $log[] = $meanwhile_nums;
     }
     return max($log);
 }
